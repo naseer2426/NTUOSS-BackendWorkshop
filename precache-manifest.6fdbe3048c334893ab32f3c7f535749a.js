6a4ec51d1d99da6dc530ac1e384608e89bf2a9eb
Naseer Ahmed Khan <naseer2426@Naseers-MacBook-Air.local>
2020-02-18 17:46:25 +0800

    Updates

---
+++ precache-manifest.6fdbe3048c334893ab32f3c7f535749a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8db73dcbb4ba398a6161bcb4d26703",
+    "url": "/Backend-Workshop/index.html"
+  },
+  {
+    "revision": "3eb7179a62999e0e7fe8",
+    "url": "/Backend-Workshop/static/css/main.cc7814c9.chunk.css"
+  },
+  {
+    "revision": "68328f5fc33afc912287",
+    "url": "/Backend-Workshop/static/js/2.50a6c9c0.chunk.js"
+  },
+  {
+    "revision": "3eb7179a62999e0e7fe8",
+    "url": "/Backend-Workshop/static/js/main.5fb78acc.chunk.js"
+  },
+  {
+    "revision": "3a1cf5c2ad21a7117e6b",
+    "url": "/Backend-Workshop/static/js/runtime~main.b7326dd6.js"
+  }
+]);
